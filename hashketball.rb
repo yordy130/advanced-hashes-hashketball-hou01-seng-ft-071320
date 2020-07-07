c8f6abbd37df73c262623594159177ca903055fb
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 07:12:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -224,10 +224,9 @@ def big_shoe_rebounds
         data.each do |k, v|
           if k[:shoe] > m_shoe
           m_shoe = k[:shoe] 
-          
-          #binding.pry
+          return k[:rebounds]
+          binding.pry
         end
-        return k[:rebounds]
       end
       end
   end
