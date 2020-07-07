e9093371d9790dc0a23190c6928b575c698d73fb
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 06:16:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,8 @@ def player_numbers(team_name)
       data.each do |k, v|
         k.map do |k1, v1|
           v1[:number]
-        #binding.pry
+          k1[:number]
+        binding.pry
        end
   end
       end
