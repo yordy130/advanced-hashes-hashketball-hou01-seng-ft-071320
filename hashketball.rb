644437636c92c8641b4b247dda4b616f5cb9c947
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 05:22:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,7 +161,19 @@ def shoe_size(player_name)
   end
 end
 
-
+def team_colors(team_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :colors
+        data.map do |data_item|
+          if data_item[:player_name] == player_name
+            return data_item[:shoe]
+              end
+            end
+          end
+      end
+  end
+end
 
 
 
