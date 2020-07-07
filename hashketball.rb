8942beb868485a9a6750b52d35073dfa3105cddd
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 05:50:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def team_colors(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
         return team_data[:colors]
-      binding.pry
+      #binding.pry
     end
   end
   end
