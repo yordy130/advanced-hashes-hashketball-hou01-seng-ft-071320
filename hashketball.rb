7562c6ad25e2cb1bde2935e9e87f885b53bd0c71
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 04:27:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,18 @@ def game_hash
 end
 
 # Write code here
-def 
+def num_points_scored(player_name)
+  binding.pry
+  game_hash.each do |location, team_data|
+    binding.pry
+    team_data.each do |attribute, data|
+      binding.pry
+      data.each do |data_item|
+          binding.pry
+      end
+    end
+  end
+end
 
 
 
